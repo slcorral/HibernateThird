f50877a3ab96d4e400b2971118c72d3023e4c9e6
MyBook <MyBook@192.168.1.135>
2017-02-19 21:13:59 +0100

    Modificación del HibernateUtil y del pom

--- EjemploHibernate/src/ejemploHibernate/HibernateUtil.java
+++ EjemploHibernate/src/ejemploHibernate/HibernateUtil.java
@@ -9,7 +9,7 @@ import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
+
 
 /**
  *
